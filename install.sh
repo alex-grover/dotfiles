ffa5399b7531d9892dfec9f3201e77aaec855329
Alex Grover <ajgrover@stanford.edu>
2014-07-26 19:24:28 -0400

    install

--- install.sh
+++ install.sh
@@ -1,31 +1,41 @@
 #!/bin/bash
 
+dotfiles=$HOME/Developer/dotfiles
+
 # customize OS X preferences
 ./osx.sh
 
 # .gitconfig
-ln -fs ./.gitconfig ~/.gitconfig
+ln -fs $dotfiles/.gitconfig ~/.gitconfig
 
 # vim
-ln -fs .vimrc ~/.vimrc
-mkdir ~/.vim_tmp
+ln -fs $dotfiles/.vimrc ~/.vimrc
+if [ ! -d ~/.vim_tmp ]; then
+    mkdir ~/.vim_tmp
+fi
 
 # sublime text
 echo "Install Sublime Text: http://www.sublimetext.com"
-ln -fs "./sublime/CoffeeScript.sublime-package" "~/Library/Application Support/Sublime Text 3/Installed Packages/CoffeeScript.sublime-package"
-ln -fs "./sublime/Theme - Soda.sublime-package" "~/Library/Application Support/Sublime Text 3/Installed Packages/Theme - Soda.sublime-package"
-ln -fs "./sublime/Default (OSX).sublime-keymap" "~/Library/Application Support/Sublime Text 3/Packages/User/Default (OSX).sublime-keymap"
-ln -fs "./sublime/Preferences.sublime-settings" "~/Library/Application Support/Sublime Text 3/Packages/User/Preferences.sublime-settings"
+ln -fs ./sublime/CoffeeScript.sublime-package ~/Library/Application\ Support/Sublime\ Text\ 3/Installed\ Packages/CoffeeScript.sublime-package
+ln -fs ./sublime/Theme\ -\ Soda.sublime-package ~/Library/Application\ Support/Sublime\ Text\ 3/Installed\ Packages/Theme\ -\ Soda.sublime-package
+ln -fs ./sublime/Default\ \(OSX\).sublime-keymap ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Default\ \(OSX\).sublime-keymap
+ln -fs ./sublime/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
 
 # zsh config files
-ln -fFs ./zsh ~/.zsh
-ln -fs ./.zshrc ~/.zshrc
-echo "Changing shell to zsh..."
-chsh -s /bin/zsh
+ln -fFs $dotfiles/zsh ~/.zsh
+ln -fs $dotfiles/.zshrc ~/.zshrc
+
+# TODO: make this work
+# if [ -z "$ZSH_VERSION" ]; then
+    chsh -s /bin/zsh
+# fi
 
 # cloudapp command line tool
-echo "Installing cloudapp gem."
-sudo gem install cloudapp_api
-ln -fs ./cloudapp.rb /usr/local/bin/cloudapp
-ln -fs ./.cloudapp ~/.cloudapp
-vim ./.cloudapp
+if ! gem list cloudapp_api -i > /dev/null 2>&1; then
+    echo "Installing cloudapp gem."
+    sudo gem install cloudapp_api
+fi
+
+ln -fs $dotfiles/cloudapp.rb /usr/local/bin/cloudapp
+cp .cloudapp ~/.cloudapp
+vim ~/.cloudapp
