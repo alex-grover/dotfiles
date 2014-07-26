66d11dbc2f279424ea66cd8100f03be7360db36f
Alex Grover <ajgrover@stanford.edu>
2014-07-26 15:51:40 -0400

    lscolors

--- zsh/theme-and-appearance.zsh
+++ zsh/theme-and-appearance.zsh
@@ -2,6 +2,8 @@
 autoload colors; colors;
 export LSCOLORS="Gxfxcxdxbxegedabagacad"
 
+ls --color -d . &>/dev/null 2>&1 && alias ls='ls --color=tty' || alias ls='ls -G'
+
 #setopt no_beep
 setopt auto_cd
 setopt multios
