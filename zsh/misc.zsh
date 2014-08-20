f4d0d2844afd57e1fb95b2f8cd5277adf7e0ccdb
Alex Grover <ajgrover@stanford.edu>
2014-08-20 14:58:53 -0400

    misc

--- zsh/misc.zsh
+++ zsh/misc.zsh
@@ -1,12 +1,6 @@
-## smart urls
+# autoescapes characters when typing URLs
 autoload -U url-quote-magic
 zle -N self-insert url-quote-magic
-
-## file rename magick
-bindkey "^[m" copy-prev-shell-word
-
-## jobs
-setopt long_list_jobs
 
 # cd dir names
 setopt auto_cd
@@ -18,5 +12,3 @@ export GREP_COLOR='1;32'
 ## pager
 export PAGER="less"
 export LESS="-R"
-
-export LC_CTYPE=$LANG
