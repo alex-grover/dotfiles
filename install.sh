b654baf968f60203008601389cf5c2ffc34aaa56
Alex Grover <ajgrover@stanford.edu>
2014-08-31 23:49:17 -0700

    delete keymap

--- install.sh
+++ install.sh
@@ -11,7 +11,6 @@ echo "Install Sublime Text: http://www.sublimetext.com"
 ln -fs $dotfiles/sublime/Package\ Control.sublime-package ~/Library/Application\ Support/Sublime\ Text\ 3/Installed\ Packages/Package\ Control.sublime-package
 ln -fs $dotfiles/sublime/CoffeeScript.sublime-package ~/Library/Application\ Support/Sublime\ Text\ 3/Installed\ Packages/CoffeeScript.sublime-package
 ln -fs $dotfiles/sublime/Theme\ -\ Soda.sublime-package ~/Library/Application\ Support/Sublime\ Text\ 3/Installed\ Packages/Theme\ -\ Soda.sublime-package
-ln -fs $dotfiles/sublime/Default\ \(OSX\).sublime-keymap ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Default\ \(OSX\).sublime-keymap
 ln -fs $dotfiles/sublime/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
 
 # zsh config files
