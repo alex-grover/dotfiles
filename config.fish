bbc5691b2c91b1ef306b9b915398f9cf8a419bfc
Alex Grover <hello@alexgrover.me>
2019-07-21 07:07:03 +0800

    Use omf prompt variable, remove extra characters

--- config.fish
+++ config.fish
@@ -1,30 +1,26 @@
 # Remove the default login message
 set fish_greeting
 
-# Remove partial directories from prompt
-function prompt_pwd
-    basename $PWD
-end
-
 # Environment variables
-set -gx EDITOR kak
-set -gx CLICOLOR 1
-set -gx LSCOLORS GxfxcxdxBxegedabagacad
-set -gx GREP_OPTIONS -r --exclude-dir=.git
+set -g EDITOR kak
+set -g CLICOLOR 1
+set -g LSCOLORS GxfxcxdxBxegedabagacad
+set -g GREP_OPTIONS -r --exclude-dir=.git
+set -g theme_short_path yes
 
 # Git aliases
 if status --is-interactive
-    abbr --add --global ga git add
-    abbr --add --global gb git branch
-    abbr --add --global gc git commit
-    abbr --add --global gd git diff
-    abbr --add --global gf git fetch
-    abbr --add --global gl git pull
-    abbr --add --global glg git log --stat --graph
-    abbr --add --global gls git ls-files
-    abbr --add --global go git open
-    abbr --add --global gp git push
-    abbr --add --global gr git remote -v
-    abbr --add --global gs git status
-    abbr --add --global gt git checkout
+    abbr -a -g ga git add
+    abbr -a -g gb git branch
+    abbr -a -g gc git commit
+    abbr -a -g gd git diff
+    abbr -a -g gf git fetch
+    abbr -a -g gl git pull
+    abbr -a -g glg git log --stat --graph
+    abbr -a -g gls git ls-files
+    abbr -a -g go git open
+    abbr -a -g gp git push
+    abbr -a -g gr git remote -v
+    abbr -a -g gs git status
+    abbr -a -g gt git checkout
 end
