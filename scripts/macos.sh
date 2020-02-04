db4046155b14e7a2420f25e0ff8121d5c58351b0
Alex Grover <hello@alexgrover.me>
2020-02-04 08:32:10 -0700

    Remove outdated macOS scripts

--- scripts/macos.sh
+++ scripts/macos.sh
@@ -3,9 +3,6 @@ echo "Customizing OS X preferences"
 # Disable the sound effects on boot
 sudo nvram SystemAudioVolume=" "
 
-# Show the ~/Library folder
-chflags nohidden ~/Library
-
 # Fast key repeat
 defaults write -g ApplePressAndHoldEnabled -bool false
 defaults write NSGlobalDomain KeyRepeat -int 1
@@ -13,5 +10,3 @@ defaults write NSGlobalDomain KeyRepeat -int 1
 # Show Safari Develop menu and hide favorites bar
 defaults write com.apple.Safari IncludeDevelopMenu -bool true
 defaults write com.apple.Safari ShowFavoritesBar -bool false
-
-echo "TODO: macOS hot corners"
