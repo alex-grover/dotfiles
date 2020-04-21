326b770369d420c8eee97bb5d16ea8348adfc732
Alex Grover <hello@alexgrover.me>
2020-04-20 19:38:25 -0600

    Add LRTimelapse through brew

--- scripts/other.sh
+++ scripts/other.sh
@@ -7,7 +7,6 @@ echo "Divvy (then run scripts/divvy.sh)"
 echo
 echo "Install the following from the web:"
 echo "DaVinci Resolve"
-echo "LRTimelapse"
 echo
 echo "Install the following locally:"
 echo "JetBrains Toolbox -> WebStorm"
