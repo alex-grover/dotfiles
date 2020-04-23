073528b9e1d55523d9e9ca85ef2b8826282fbaf6
Alex Grover <hello@alexgrover.me>
2020-04-22 19:12:45 -0600

    Use DaVinci Resolve from MAS

--- scripts/mas.sh
+++ scripts/mas.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 
 mas install 1440147259 # AdGuard for Safari
+mas install 571213070 # DaVinci Resolve
 mas install 413857545 # Divvy
 mas install 409183694 # Keynote
 mas install 409203825 # Numbers
--- scripts/other.sh
+++ scripts/other.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env sh
 
 echo "Install the following from the web:"
-echo "DaVinci Resolve"
 echo "Capture One"
 echo "Affinity Photo"
