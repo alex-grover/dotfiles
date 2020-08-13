dc8eb84b523e180e39d390ccb5f38c49ea2c3065
Alex Grover <hello@alexgrover.me>
2020-08-13 15:33:36 -0600

    Add hot corner preferences

--- scripts/macos.sh
+++ scripts/macos.sh
@@ -12,3 +12,12 @@ defaults write NSGlobalDomain KeyRepeat -int 1
 # Show Safari Develop menu and hide favorites bar
 defaults write com.apple.Safari IncludeDevelopMenu -bool true
 defaults write com.apple.Safari ShowFavoritesBar -bool false
+
+# Hot corners
+defaults write com.apple.dock wvous-br-corner -int 2 # Mission Control
+defaults write com.apple.dock wvous-br-modifier -int 0
+defaults write com.apple.dock wvous-tl-corner -int 10 # Put Display to Sleep
+defaults write com.apple.dock wvous-tl-corner -int 0
+defaults write com.apple.dock wvous-tr-corner -int 4 # Desktop
+defaults write com.apple.dock wvous-tr-corner -int 0
+killall Dock
