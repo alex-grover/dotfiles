74e6f5185611f97c3fe325e3cf2aac1fb2190d42
Alex Grover <hello@alexgrover.me>
2020-10-25 19:38:43 -0700

    Remove unnecessary and incorrect mas signin
    Closes #1

--- scripts/homebrew.sh
+++ scripts/homebrew.sh
@@ -4,6 +4,4 @@ echo "Installing Homebrew"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
 echo "Installing Homebrew packages"
-read -p "Email (for App Store sign in): " email
-mas signin $email
 brew bundle
