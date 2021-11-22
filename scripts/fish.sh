b9e8b4caa775f0385702c1a339911dfe7936d022
Alex Grover <hello@alexgrover.me>
2021-11-22 15:12:44 -0800

    Use updated OMF install link

--- scripts/fish.sh
+++ scripts/fish.sh
@@ -8,5 +8,5 @@ echo "Symlinking fish config file"
 ln -s $(pwd)/config/config.fish ~/.config/fish
 
 echo "Installing oh-my-fish"
-curl -L https://get.oh-my.fish | fish
+curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
 omf install https://github.com/jorgebucaran/autopair.fish
