2a83b07cc44488630279e4cc05ddb09207827134
Alex Grover <hello@alexgrover.me>
2022-05-31 15:31:12 -0700

    Update `gh-dash` config dir
    https://github.com/dlvhdr/gh-dash/issues/82

--- scripts/gh-dash.sh
+++ scripts/gh-dash.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 echo "Installing \`gh dash\`"
-gh extension install dlvhdr/gh-dash
-CONFIG_DIR=$HOME/Library/Application\ Support/gh-dash
+#gh extension install dlvhdr/gh-dash
+CONFIG_DIR=$HOME/.config/gh-dash
 mkdir -p "$CONFIG_DIR"
 ln -s $(pwd)/config/gh-dash.yml "$CONFIG_DIR/config.yml"
