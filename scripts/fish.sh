177d7db776b8ad56d5d1cfd8ad3357eda0c3f6c7
Alex Grover <hello@alexgrover.me>
2025-04-27 20:13:24 -0400

    Remove starship config

--- scripts/fish.sh
+++ scripts/fish.sh
@@ -10,7 +10,6 @@ echo "Symlinking fish configuration files"
 CONFIG_DIR=$HOME/.config/fish
 mkdir -p $CONFIG_DIR
 ln -fs $(pwd)/config/config.fish $CONFIG_DIR
-ln -fs $(pwd)/config/starship.toml $HOME/.config
 
 echo "Adding brew bin dir to path"
 fish -c "fish_add_path (brew --prefix)/bin"
